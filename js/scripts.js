2b03dd6f2e6c5dd4f7c3de59f9d95438f2e8b177
Levi Bibo and Steve Burton <levi@>
2016-10-24 15:38:06 -0700

    make javascript word sorter ignore capitalization and punctuation.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,40 @@
 var words = [];
 var totals = [];
+var punctuation = [".", ",", "?", "!", ";", ":", "-", "_", '"', "/"];
 var sentence;
+var tempWord;
 
 $(document).ready(function(){
   $("form").submit(function(event) {
     var wordIndex = -1;
     var wordCount = 1;
     sentence = $("#input").val().split(" ");
-    sentence.sort();
-    sentence.forEach(function(word){
-      if (words.indexOf(word) != -1) {
+    newSentence = sentence.map(function(word){
+      return word.toUpperCase();
+    })
+    newSentence.sort();
+    newSentence.forEach(function(word){
+      tempWord = word;
+      punctuation.forEach(function(mark) {
+        if (word.indexOf(mark) != -1) {
+          tempWord = word.replace(mark, "");
+        };
+      });
+
+      if (words.indexOf(tempWord) != -1) {
         wordCount++;
         totals[wordIndex] = wordCount;
       } else {
         wordIndex++;
         wordCount = 1;
-        words.push(word);
+        words.push(tempWord);
         totals.push(wordCount);
       };
     });
-    console.log(words);
-    console.log(totals);
+
+    words.forEach(function(word) {
+      $("ul").append("<li>" + word + " " + totals[words.indexOf(word)] + "</li>")
+    })
 
     event.preventDefault();
   });
